63480c1c4a439c02efddf9c33eea979fb6754478
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-27 20:03:06 +0530

    Create Print Without Space.cpp

---
+++ Basic/Print Without Space.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a;
+    getline(cin, a);
+    string b;
+    getline(cin, b);
+    // Print in the required format and a new line
+    cout << a << b ;
+    return 0;
+}
