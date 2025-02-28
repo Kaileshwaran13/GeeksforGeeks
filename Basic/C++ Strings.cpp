ad3863b4cd0ffa09f17fed5f46f72d0e480e3c30
Kaileshwaran <128310801+Kaileshwaran13@users.noreply.github.com>
2025-02-28 18:09:35 +0530

    Create C++ Strings.cpp

---
+++ Basic/C++ Strings.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string conCat(string a , string b);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a,b;
+        cin>>a>>b;
+        cout<<conCat(a,b)<<endl;
+        cout << "~" << "\n";
+    }
+    return 0;
+}
+
+string conCat(string s1 , string s2)
+{
+    return s1 + s2;
+}
